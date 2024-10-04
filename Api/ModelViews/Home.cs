5b89375ee86918127bad847c9493863357fa33a4
GusSalgado <gustavo.salgado@farmarcas.com.br>
2024-10-04 17:58:15 -0300

    feat(home) add a home page

---
+++ Api/ModelViews/Home.cs
@@ -0,0 +1,8 @@
+﻿namespace minimal_api.ModelViews
+{
+    public struct Home
+    {
+        public string Message { get => "Welcome to Vehicles API - Minimal API"; }
+        public string Documentation { get => "/swagger"; }
+    }
+}
