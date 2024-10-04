514d740bff0e57bca34705e5b63dbb80803b2f07
GusSalgado <gustavo.salgado@farmarcas.com.br>
2024-10-04 11:29:48 -0300

    feat(app): domain and infra

---
+++ Infraestructure/Services/IAdministratorService.cs
@@ -0,0 +1,25 @@
+﻿using MinimalApi.Domain.DTOs;
+using MinimalApi.Domain.Entities;
+using MinimalApi.Domain.Interfaces;
+using MinimalApi.Infraestructure.Db;
+
+namespace MinimalApi.Infrastructure.Services
+{
+    public class AdministratorService : IAdministratorService
+    {
+        private readonly Context _context;
+
+        public AdministratorService(Context context)
+        {
+            _context = context;
+        }
+
+        Administrator? Login(LoginDTO loginDTO)
+        {
+            if (_context.Administrators.Where(adm => adm.Email == loginDTO.Email && adm.Password == loginDTO.Password).Any()
+                return _context.Administrators.Where(adm => adm.Email == loginDTO.Email && adm.Password == loginDTO.Password).FirstOrDefault();
+            else
+                return null;
+        }
+    }
+}
